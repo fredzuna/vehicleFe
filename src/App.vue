bffddaa12f069caccb82c55d1289f0395a46ad17
Fredy <fredzuna@hotmail.com>
2024-08-17 23:22:21 -0400

    empty vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import VehiclePricing from './components/VehiclePricing.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Vehicle preci calculator</h1>
+  </div>
+  <VehiclePricing />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/VehiclePricing.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineProps()
+</script>
+
+<template>
+  <div class="card">
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
